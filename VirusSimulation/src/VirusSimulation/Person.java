63b3125813c0b424ec07418986cfdb5a189db09f
bramleentjens <bramleentjens@gmail.com>
2017-05-23 11:24:47 +0200

    Add files via upload

---
+++ VirusSimulation/src/VirusSimulation/Person.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VirusSimulation;
+
+/**
+ *
+ * @author 010918
+ */
+public class Person {
+    
+}
