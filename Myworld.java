b81e1efee2d5d60bbed56e984128ba7608eb78c9
bramleentjens <bramleentjens@gmail.com>
2017-05-23 11:21:34 +0200

    Add files via upload

---
+++ Myworld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myworld;
+
+/**
+ *
+ * @author 010918
+ */
+public class Myworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int hello;
+        
+        hello = 6;
+        
+        System.out.println("Hello World");
+        System.out.println("Bla");
+    }
+}
